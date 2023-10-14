fff4d67705417d9492341c4e52b467d21d5a142a
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-10-14 16:54:24 +0200

    Connected online database (MongoDB Atlas)

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ const Users = Models.User;
 
 
 // connected mongodb or integrated b/w REST API to data layer
-mongoose.connect('mongodb://127.0.0.1:27017/myFlix', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://127.0.0.1:27017/myFlix', {useNewUrlParser: true, useUnifiedTopology: true});
+// to connect the online database (mongoDB Atlas)
+mongoose.connect('mongodb+srv://myFlixDbAdmin:Geet%40123$@myflix.wif2ved.mongodb.net/myFlix?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 
