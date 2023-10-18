923cd06f576bf24fefceb84436b003d4cc3ac3a4
Badr Ramadhan Aheebwa <45152319+raheebwa@users.noreply.github.com>
2023-10-18 13:19:50 +0300

    refactor: Refactored schema definitions
    Refactored schema definitions to establish relationships between Movies, Genres, Directors, Actors, and Users using Mongoose references

--- models.js
+++ models.js
@@ -1,81 +1,62 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// schema defines for document in movie collection
-let movieSchema = mongoose.Schema(
-    {
-    Title : {type: String, required: true},
-    Description: { type: String, required:true},
-    Genres: { 
-        name: String,
-        description: String
-     },
-    Directors:{
-        name: String,
-        bio : String,
-        birth_year: Date,
-        death_year: Date
-    },
-    Actors:[{ 
-        name: String,
-        birth_date: Date
-    }],
+// Schema for movies
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genres: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],  // Reference to Genre schema
+    Directors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Director' }],  // Reference to Director schema
+    Actors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Actor' }],  // Reference to Actor schema
     imageURL: String,
     Release_Date: Date,
-    Ratings: Number, 
+    Ratings: Number,
     Featured: Boolean
-}
-
-); 
-let genreSchema = mongoose.Schema(
-    {
-        name: String,
-        description: String
-    }
-);
-
-let directorSchema = mongoose.Schema(
-    {
-        name: String,
-        bio : String,
-        birth_year: Date,
-        death_year: Date
-    }
-);
-
-let actorSchema= mongoose.Schema(
-    {
-        name: String,
-        birth_date: Date
-    }
-);
-
-let userSchema = mongoose.Schema(
-    {
-        username: {type:String, required: true},
-        password: {type: String, required: true},
-        email: {type: String, required: true},
-        birth_date: Date,
-        favorite_movies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
-userSchema.statics.hashPassword = (password)=>{
+
+// Schema for genres
+let genreSchema = mongoose.Schema({
+    name: String,
+    description: String
+});
+
+// Schema for directors
+let directorSchema = mongoose.Schema({
+    name: String,
+    bio: String,
+    birth_year: Date,
+    death_year: Date
+});
+
+// Schema for actors
+let actorSchema = mongoose.Schema({
+    name: String,
+    birth_date: Date
+});
+
+// Schema for users
+let userSchema = mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    birth_date: Date,
+    favorite_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]  // Reference to Movie schema
+});
+
+// Password hashing and validation methods for User schema
+userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 12);
 };
-userSchema.methods.validatePassword = function(password){
+userSchema.methods.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.password);
 };
 
-// these will create a collection of db.movies & db.users
-let Movie= mongoose.model('Movie', movieSchema);
-let Genre= mongoose.model('Genre', genreSchema);
+// Model creation
+let Movie = mongoose.model('Movie', movieSchema);
+let Genre = mongoose.model('Genre', genreSchema);
 let Director = mongoose.model('Director', directorSchema);
 let Actor = mongoose.model('Actor', actorSchema);
-let User= mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema);
 
-
-// exports these two models to index.js
-module.exports.Movie = Movie;
-module.exports.Genre = Genre;
-module.exports.Director= Director;
-module.exports.Actor= Actor;
-module.exports.User = User;
+// Exporting the models
+module.exports = { Movie, Genre, Director, Actor, User };
